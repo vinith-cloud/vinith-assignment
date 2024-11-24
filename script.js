7d51db59aa7d920fa48cadeed903db26908a3e57
vinith-cloud <vinithkumar0710@gmail.com>
2024-11-24 13:54:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const btnelement = document.querySelector('button');
-const spanelement= document.getElementById('updated content');
-btnelement.onclick=function(){
-    const yourname=prompt("please enter your name.");
-    spanelement.textContent=yourname;}
